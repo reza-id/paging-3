7cd4fc5c952077e59f3dd61b8d4063f98b5c0850
agus <reza.agustiana@gmail.com>
2020-10-08 12:14:25 +0700

    Dependency Injection with Hilt

---
+++ app/src/main/java/dev/cianjur/pagging3/CianjurApplication.kt
@@ -0,0 +1,7 @@
+package dev.cianjur.pagging3
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CianjurApplication : Application()
--- app/src/main/java/dev/cianjur/pagging3/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/dev/cianjur/pagging3/ui/gallery/GalleryFragment.kt
@@ -1,8 +1,10 @@
 package dev.cianjur.pagging3.ui.gallery
 
 import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
 import dev.cianjur.pagging3.R
 
+@AndroidEntryPoint
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 
 }
