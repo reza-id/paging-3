2ef4d1dfa45642ecf93bfb647621192cba82bc59
agus <reza.agustiana@gmail.com>
2020-10-08 14:19:39 +0700

    Handling Process Death

--- app/src/main/java/dev/cianjur/pagging3/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/dev/cianjur/pagging3/ui/gallery/GalleryViewModel.kt
@@ -1,7 +1,8 @@
 package dev.cianjur.pagging3.ui.gallery
 
+import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.switchMap
 import androidx.lifecycle.viewModelScope
@@ -9,14 +10,16 @@ import androidx.paging.cachedIn
 import dev.cianjur.pagging3.data.UnsplashRepository
 
 class GalleryViewModel @ViewModelInject constructor(
-    private val repository: UnsplashRepository
+    private val repository: UnsplashRepository,
+    @Assisted state: SavedStateHandle
 ) : ViewModel() {
 
     companion object {
+        private const val CURRENT_QUERY = "current_query"
         private const val DEFAULT_QUERY = "cats"
     }
 
-    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+    private val currentQuery = state.getLiveData(CURRENT_QUERY, DEFAULT_QUERY)
 
     val photos = currentQuery.switchMap { queryString ->
         repository.getSearchResults(queryString).cachedIn(viewModelScope)
