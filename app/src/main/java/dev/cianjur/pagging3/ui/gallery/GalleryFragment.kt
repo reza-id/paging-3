9bad6dcc6f59ff0df2d0f3ee025f67fce5774bf3
agus <reza.agustiana@gmail.com>
2020-10-08 13:02:22 +0700

    PagingSource & PagingData

--- app/src/main/java/dev/cianjur/pagging3/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/dev/cianjur/pagging3/ui/gallery/GalleryFragment.kt
@@ -1,10 +1,22 @@
 package dev.cianjur.pagging3.ui.gallery
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import dev.cianjur.pagging3.R
 
 @AndroidEntryPoint
 class GalleryFragment : Fragment(R.layout.fragment_gallery) {
 
+    private val viewModel by viewModels<GalleryViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.photos.observe(viewLifecycleOwner) {
+            // need paging data adapter
+        }
+    }
 }
--- app/src/main/java/dev/cianjur/pagging3/ui/gallery/GalleryViewModel.kt
+++ app/src/main/java/dev/cianjur/pagging3/ui/gallery/GalleryViewModel.kt
@@ -1,9 +1,28 @@
 package dev.cianjur.pagging3.ui.gallery
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import dev.cianjur.pagging3.data.UnsplashRepository
 
 class GalleryViewModel @ViewModelInject constructor(
     private val repository: UnsplashRepository
-) : ViewModel()
+) : ViewModel() {
+
+    companion object {
+        private const val DEFAULT_QUERY = "cats"
+    }
+
+    private val currentQuery = MutableLiveData(DEFAULT_QUERY)
+
+    val photos = currentQuery.switchMap { queryString ->
+        repository.getSearchResults(queryString).cachedIn(viewModelScope)
+    }
+
+    fun searchPhotos(query: String) {
+        currentQuery.value = query
+    }
+}
