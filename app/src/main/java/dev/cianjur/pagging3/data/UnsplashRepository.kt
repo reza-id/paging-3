1f52e09ee07d904b7ace4c9ff3054dd3b0fc7af9
agus <reza.agustiana@gmail.com>
2020-10-08 12:51:51 +0700

    PagingSource & PagingData

--- app/src/main/java/dev/cianjur/pagging3/data/UnsplashRepository.kt
+++ app/src/main/java/dev/cianjur/pagging3/data/UnsplashRepository.kt
@@ -1,9 +1,18 @@
 package dev.cianjur.pagging3.data
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.liveData
 import dev.cianjur.pagging3.api.UnsplashApi
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class UnsplashRepository @Inject constructor(private val unsplashApi: UnsplashApi) {
+
+    fun getSearchResults(query: String) =
+        Pager(
+            config = PagingConfig(pageSize = 20, maxSize = 100, enablePlaceholders = false),
+            pagingSourceFactory = { UnsplashPagingSource(unsplashApi, query) }
+        ).liveData
 }
