261acc2692b7a697c384027cfc8880ad61279a1d
agus <reza.agustiana@gmail.com>
2020-10-08 13:47:22 +0700

    Search Functionality

--- app/src/main/java/dev/cianjur/pagging3/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/dev/cianjur/pagging3/ui/gallery/GalleryFragment.kt
@@ -1,7 +1,10 @@
 package dev.cianjur.pagging3.ui.gallery
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
@@ -34,6 +37,29 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
         viewModel.photos.observe(viewLifecycleOwner) {
             adapter.submitData(viewLifecycleOwner.lifecycle, it)
         }
+
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_gallery, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                query?.let {
+                    binding.recyclerView.scrollToPosition(0)
+                    viewModel.searchPhotos(it)
+                    searchView.clearFocus()
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?) = true
+        })
     }
 
     override fun onDestroyView() {
